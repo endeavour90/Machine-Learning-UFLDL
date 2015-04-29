9cbc5fc4fccc12db2a12430fa2838e5f6caea734
Fernando Pratama <endeavour90@gmail.com>
2015-04-29 18:15:57 +0700

    Finish computation of cost function and its gradient for logistic regression

--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -6,7 +6,8 @@ function [f,g] = logistic_regression(theta, X,y)
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
   %
-
+  
+  n=size(X,1);
   m=size(X,2);
   
   % initialize objective value and gradient.
@@ -22,3 +23,15 @@ function [f,g] = logistic_regression(theta, X,y)
   %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
   %
 %%% YOUR CODE HERE %%%
+
+for i = 1:m
+    hyp = hypothesis(theta, X(:,i));
+    f = f - y(i)*log(hyp) - (1-y(i))*log(1-hyp);
+    g = g + X(:,i).*(ones(n,1)*(hyp-y(i)));
+end
+
+end
+
+function h = hypothesis(theta, x)
+    h = 1./(1 + exp(-theta'*x));
+end
