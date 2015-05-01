8305cce47b1a99c4cd955971846227ce49f99a30
Fernando Pratama <endeavour90@gmail.com>
2015-05-01 10:29:47 +0700

    Implementing vectorised linear regression

--- ex1/linear_regression_vec.m
+++ ex1/linear_regression_vec.m
@@ -6,11 +6,6 @@ function [f,g] = linear_regression_vec(theta, X,y)
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The target value for each example.  y(j) is the target for example j.
   %
-  m=size(X,2);
-  
-  % initialize objective value and gradient.
-  f = 0;
-  g = zeros(size(theta));
 
   %
   % TODO:  Compute the linear regression objective function and gradient 
@@ -18,3 +13,7 @@ function [f,g] = linear_regression_vec(theta, X,y)
   %        Store the objective function value in 'f', and the gradient in 'g'.
   %
 %%% YOUR CODE HERE %%%
+
+f = 0.5.*(X'*theta - y')'*(X'*theta - y');
+g = X*X'*theta - X*y';
+end
